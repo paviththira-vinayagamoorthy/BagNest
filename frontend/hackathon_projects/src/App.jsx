879c1e856a9b84164f7fdf3b46adacbc802c25bb
anetresia <patrickanetresia@gmail.com>
2026-09-24 10:55:46 +0530

    first ui working checked

--- frontend/hackathon_projects/src/App.jsx
+++ frontend/hackathon_projects/src/App.jsx
@@ -5,11 +5,29 @@ import viteLogo from './assets/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
     <>
-     
+      <div className="min-vh-100 d-flex align-items-center">
+      
+      <div className="container text-center">
+        
+        <h1 className="display-4 fw-bold text-success">
+          BagNest
+        </h1>
+
+        <p className="lead">
+          Smart luggage storage for travellers.
+        </p>
+
+        <button className="btn btn-success px-4">
+          Get Started
+        </button>
+
+      </div>
+
+    </div>
     </>
   )
 }
